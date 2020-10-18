56cbc6501e95ef7ffb6b512a27351b6158cff441
Arnau <agomez1f@gmail.com>
2020-10-18 17:39:15 +0200

    ✨Added user login

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!--This is the DASHBOARD, the user's personal space. -->
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fbcn7-1.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.fbcn7-1.fna.fbcdn.net&_nc_ohc=syaQJRHjW98AX-Mu9ex&oh=bee928c281b6354b658f7a3d5db07677&oe=5FB58C67" alt="" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>freeCodeCamp</h1>
+            </div>
+            <div class="d-flex">
+                <div class=""><strong>153</strong> posts</div>
+                <div class="px-5"><strong>23k</strong> followers</div>
+                <div class=""><strong>212</strong> following</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
